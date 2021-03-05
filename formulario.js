c3d790fbb814677a6957965bf3adcee1d3dda8e5
Leonardo Treviso <leonardosistemas@outlook.com>
2021-03-05 20:30:47 -0300

    Criei a função para salvar dados no objeto javascript

--- formulario.js
+++ formulario.js
@@ -1,3 +1,13 @@
 let objCadastro = {}
 let arrayCadastro = []
 
+function salvardadosobjeto(){
+    objCadastro = {
+        "Nome" : document.getElementById('txtNome').value,
+        "Email" : document.getElementById('txtEmail').value,
+        "Endereco" : document.getElementById('txtEndereco').value,
+        "Endereco2" : document.getElementById('txtEndereco2').value,
+        "Telefone" : document.getElementById('txtTelefone').value,
+        "Celular" : document.getElementById('txtCelular').value
+    }    
+}
